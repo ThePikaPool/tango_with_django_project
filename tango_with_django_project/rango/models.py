665d1d05b814fad72893f0876cb7b8bb98d56407
--replace <2975682c@student.gla.ac.uk>
2026-02-13 00:37:56 +0000

    Completed chapter 5.

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+# alright, starting chapter 5. i don't have much time till this is due,
+# might have to pull an all nighter
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=128, unique=True)
+    # guessing the above is making a database field that's the primary key
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+    
+
+class Page(models.Model):
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # oh it's just CS1F stuff
+    # bravo UofG, the preliminary courses now make sense
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
